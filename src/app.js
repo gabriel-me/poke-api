77764754db505bd278f5772904628e2c19ca72b1
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-12-11 13:03:08 -0300

    Refatorated code

--- src/app.js
+++ src/app.js
@@ -6,11 +6,11 @@ const userRoute = require('./routes/user')
 const pokemonRoute = require('./routes/pokemon')
 const authenticateRoute = require('./routes/authenticate')
 
-database.connect()
-
 const port = 8082
 const app = express()
 const server = http.createServer(app)
+
+database.connect()
 
 app.use(cors())
 app.use(express.json())
