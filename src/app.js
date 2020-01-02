712b4a92233b2ac844d37b4f4db5d9965e4fa093
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2020-01-02 14:36:45 -0300

    Path config

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const http = require('http')
 const database = require('./database/index')
@@ -16,6 +17,7 @@ database.connect()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.use(authenticateRoute)
 app.use(userRoute)
