5206a4c8381ce2491f98071722b7c7dd76586610
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2020-01-02 14:28:59 -0300

    Config path

--- src/middlewares/handleUpload.js
+++ src/middlewares/handleUpload.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
   destination: (request, file, callback) => {
-    callback(null, path.resolve(__dirname, '..', '..', 'uploads'))
+    callback(null, path.resolve(__dirname, '..', 'uploads'))
   },
   filename: (request, file, callback) => {
     callback(null, `${Date.now()}-${file.originalname}`)
